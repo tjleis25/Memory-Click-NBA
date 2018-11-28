63e71882a7f93764575ade109ecb034480698ede
Thomas Leis <tjleis25@gmail.com>
2018-11-28 16:35:27 -0500

    Updates

---
+++ precache-manifest.a781c4be2128efb7b51ec69219ae732e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "891237b59f67317baff7",
+    "url": "/memory-click-nba/static/js/runtime~main.891237b5.js"
+  },
+  {
+    "revision": "3515a37a580251c2babb",
+    "url": "/memory-click-nba/static/js/main.3515a37a.chunk.js"
+  },
+  {
+    "revision": "705380186d4e6d38d8b0",
+    "url": "/memory-click-nba/static/js/1.70538018.chunk.js"
+  },
+  {
+    "revision": "3515a37a580251c2babb",
+    "url": "/memory-click-nba/static/css/main.42c5f39e.chunk.css"
+  },
+  {
+    "revision": "2507543391d5cca88bab7209e3c397f3",
+    "url": "/memory-click-nba/index.html"
+  }
+];
